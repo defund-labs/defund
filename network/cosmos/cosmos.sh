dd3acad07cd42c71a43365bb6210fb8c95ff50b3
Joe Schnetzler <schnetzlerjoe@gmail.com>
2022-04-05 04:45:42 +0000

    interquery touchups

--- network/cosmos/cosmos.sh
+++ network/cosmos/cosmos.sh
@@ -3,7 +3,7 @@
 # Install the specialized, ics-27 enabled gaiad binary
 cd $HOME
 yes | rm -r gaia
-git clone https://github.com/schnetzlerjoe/gaia
+git clone https://github.com/cosmos/gaia
 cd gaia
 make install
 sudo cp $GOPATH/bin/gaiad /usr/local/bin
