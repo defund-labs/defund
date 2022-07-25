c60cd7616516febd747fde086693e0f6c3e36504
Joe Schnetzler <schnetzlerjoe@gmail.com>
2022-07-24 22:25:28 -0500

    edit

--- testutil/keeper/etf.go
+++ testutil/keeper/etf.go
@@ -8,7 +8,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/store"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 	"github.com/defund-labs/defund/x/etf/keeper"
 	"github.com/defund-labs/defund/x/etf/types"
 	"github.com/stretchr/testify/require"
@@ -32,7 +31,6 @@ func EtfKeeper(t testing.TB) (*keeper.Keeper, sdk.Context) {
 		codec.NewProtoCodec(registry),
 		storeKey,
 		memStoreKey,
-		paramtypes.Subspace{},
 		nil,
 		nil,
 		nil,
