2b297e04babe72f792f86aec75ac71646136cf48
Joe S <schnetzlerjoe@gmail.com>
2023-03-19 20:37:59 -0500

    Change optimize

--- devtools/optimize.sh
+++ devtools/optimize.sh
@@ -7,9 +7,9 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   sed -i 's/max_packet_msg_payload_size =.*/max_packet_msg_payload_size = 10240/g' $CONFIG_PATH
   sed -i 's/send_rate =.*/send_rate = 20480000/g' $CONFIG_PATH
   sed -i 's/recv_rate =.*/recv_rate = 20480000/g' $CONFIG_PATH
-  sed -i 's/timeout_prevote =.*/timeout_prevote = "100ms"/g' $CONFIG_PATH
-  sed -i 's/timeout_precommit =.*/timeout_precommit = "100ms"/g' $CONFIG_PATH
-  sed -i 's/timeout_commit =.*/timeout_commit = "100ms"/g' $CONFIG_PATH
+  sed -i 's/timeout_prevote =.*/timeout_prevote = "130ms"/g' $CONFIG_PATH
+  sed -i 's/timeout_precommit =.*/timeout_precommit = "130ms"/g' $CONFIG_PATH
+  sed -i 's/timeout_commit =.*/timeout_commit = "130ms"/g' $CONFIG_PATH
   sed -i 's/skip_timeout_commit =.*/skip_timeout_commit = false/g' $CONFIG_PATH
 elif [[ "$OSTYPE" == "darwin"* ]]; then
   sed -i '' 's/max_num_inbound_peers =.*/max_num_inbound_peers = 150/g' $CONFIG_PATH
@@ -17,9 +17,9 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
   sed -i '' 's/max_packet_msg_payload_size =.*/max_packet_msg_payload_size = 10240/g' $CONFIG_PATH
   sed -i '' 's/send_rate =.*/send_rate = 20480000/g' $CONFIG_PATH
   sed -i '' 's/recv_rate =.*/recv_rate = 20480000/g' $CONFIG_PATH
-  sed -i '' 's/timeout_prevote =.*/timeout_prevote = "100ms"/g' $CONFIG_PATH
-  sed -i '' 's/timeout_precommit =.*/timeout_precommit = "100ms"/g' $CONFIG_PATH
-  sed -i '' 's/timeout_commit =.*/timeout_commit = "100ms"/g' $CONFIG_PATH
+  sed -i '' 's/timeout_prevote =.*/timeout_prevote = "130ms"/g' $CONFIG_PATH
+  sed -i '' 's/timeout_precommit =.*/timeout_precommit = "130ms"/g' $CONFIG_PATH
+  sed -i '' 's/timeout_commit =.*/timeout_commit = "130ms"/g' $CONFIG_PATH
   sed -i '' 's/skip_timeout_commit =.*/skip_timeout_commit = false/g' $CONFIG_PATH
 else
   printf "Platform not supported, please ensure that the following values are set in your config.toml:\n"
@@ -30,9 +30,9 @@ else
   printf "\t send_rate = 20480000\n"
   printf "\t recv_rate = 20480000\n"
   printf "###         Consensus Configuration Options         ###\n"
-  printf "\t timeout_prevote = \"100ms\"\n"
-  printf "\t timeout_precommit = \"100ms\"\n"
-  printf "\t timeout_commit = \"100ms\"\n"
+  printf "\t timeout_prevote = \"130ms\"\n"
+  printf "\t timeout_precommit = \"130ms\"\n"
+  printf "\t timeout_commit = \"130ms\"\n"
   printf "\t skip_timeout_commit = false\n"
   exit 1
 fi
