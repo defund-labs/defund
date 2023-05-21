0477d54f26467ee74bd7b4963a9f539d623e477a
NodesLLS <133574162+NodesLLS@users.noreply.github.com>
2023-05-21 14:18:21 +0300

    Update init.sh
        Added double quotes around variable references to handle cases with spaces or special characters in the directory paths.
        Used the source command instead of the dot (.) notation for sourcing the shell variables file.
        Added comments to explain the purpose of each section.
        Maintained consistent indentation and spacing for better code readability.

--- network/relayer/init.sh
+++ network/relayer/init.sh
@@ -2,19 +2,19 @@
 set -e
 
 # Load shell variables
-. ./network/relayer/variables.sh
+source ./network/relayer/variables.sh
 
 # Delete previous config directory if present
-if [ -d  $RELAYER_DIRECTORY/config ]; then
-    rm -r $RELAYER_DIRECTORY/config
+if [ -d "$RELAYER_DIRECTORY/config" ]; then
+    rm -r "$RELAYER_DIRECTORY/config"
 fi
 
 # Init the config
-echo "Initiating config file..."
-rly config init --home $RELAYER_DIRECTORY
+echo "Initializing config file..."
+rly config init --home "$RELAYER_DIRECTORY"
 
 # Move already provided config file to initialized config directory
-rm $CONFIG_DIRECTORY/config.yaml
-cp $RELAYER_DIRECTORY/config.yaml $CONFIG_DIRECTORY
+rm "$CONFIG_DIRECTORY/config.yaml"
+cp "$RELAYER_DIRECTORY/config.yaml" "$CONFIG_DIRECTORY"
 
 sleep 2
