b70e57e1ab4edb190e2e3bca4c1f48581c4570a1
schnetzlerjoe <schnetzlerjoe@gmail.com>
2024-08-13 18:42:28 +0000

    use baseapp context

--- testutil/keeper/dex.go
+++ testutil/keeper/dex.go
@@ -7,24 +7,13 @@ import (
 	"testing"
 
 	"cosmossdk.io/log"
-	"cosmossdk.io/store"
-	"cosmossdk.io/store/metrics"
-	storetypes "cosmossdk.io/store/types"
-	cmtproto "github.com/cometbft/cometbft/proto/tendermint/types"
 	dbm "github.com/cosmos/cosmos-db"
 	simtestutil "github.com/cosmos/cosmos-sdk/testutil/sims"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/stretchr/testify/require"
 )
 
 func TestDexKeeper(t testing.TB) (keeper.Keeper, sdk.Context, *app.App) {
-	storeKey := storetypes.NewKVStoreKey(types.StoreKey)
-
 	db := dbm.NewMemDB()
-	stateStore := store.NewCommitMultiStore(db, log.NewNopLogger(), metrics.NewNoOpMetrics())
-	stateStore.MountStoreWithDB(storeKey, storetypes.StoreTypeIAVL, db)
-	require.NoError(t, stateStore.LoadLatestVersion())
-
 	app := app.New(
 		log.NewNopLogger(),
 		db,
@@ -35,7 +24,7 @@ func TestDexKeeper(t testing.TB) (keeper.Keeper, sdk.Context, *app.App) {
 
 	k := app.DexKeeper
 
-	ctx := sdk.NewContext(stateStore, cmtproto.Header{}, false, log.NewNopLogger())
+	ctx := app.BaseApp.NewContext(true)
 
 	// Initialize params
 	k.SetParams(ctx, types.DefaultParams())
